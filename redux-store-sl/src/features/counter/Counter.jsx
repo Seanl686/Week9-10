772f3b4faa9c7f0dab3c6da444854f60fb7a086c
Sean Lebida <seanmlebida@cmconnect.cmcc.edu>
2025-04-04 10:56:42 -0400

    Refactor counter feature: add styles for Counter component and improve button accessibility

--- redux-store-sl/src/features/counter/Counter.jsx
+++ redux-store-sl/src/features/counter/Counter.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './counterSlice'
+import styles from './Counter.module.css'
 
 export function Counter() {
   const count = useSelector((state) => state.counter.value)
@@ -9,11 +10,17 @@ export function Counter() {
   return (
     <div>
       <div>
-        <button onClick={() => dispatch(increment())}>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
           Increment
         </button>
         <span>{count}</span>
-        <button onClick={() => dispatch(decrement())}>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
           Decrement
         </button>
       </div>
--- redux-store-sl/src/features/counter/counterSlice.jsx
+++ redux-store-sl/src/features/counter/counterSlice.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './counterSlice'
+import { createSlice } from '@reduxjs/toolkit'
 
-export function Counter() {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes.
+      // Also, no return statement is required from these functions.
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload
+    },
+  },
+})
 
-  return (
-    <div>
-      <div>
-        <button onClick={() => dispatch(increment())}>
-          Increment
-        </button>
-        <span>{count}</span>
-        <button onClick={() => dispatch(decrement())}>
-          Decrement
-        </button>
-      </div>
-    </div>
-  )
-}
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+
+export default counterSlice.reducer
